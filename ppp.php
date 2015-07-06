db3733a33e2cb2cd8312180797b8b200a8ce8fea
horollgame <ruskikh_@mail.ua>
2015-07-07 00:42:41 +0300

    Форма для регистрации полей

---
+++ ppp.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Заходете если что!</h1>
+<p>Пожалуйста, введите ниже свои данные:</p>
+<form action="type.php" method="POST">
+<fieldset>
+<label for="first_name">Имя:</label>
+<input type="text" name="first_name" size="20" required /><br />
+<label for="last_name">Фамилия:</label>
+<input type="text" name="last_name" size="20" required/><br />
+<label for="email">Адрес электронной почты:</label>
+<input type="text" name="email" size="50"required /><br />
+<input type="submit" value="Вступить в клуб" />
+<input type="reset" value="Очистить и начать все сначала" />
+</fieldset>
+</form>
+</body>
+</html>
